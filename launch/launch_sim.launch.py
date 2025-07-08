e489164813fe912f38085ed619432c51311a48d6
Brunogabrl <brunogabrltr@gmail.com>
2025-07-08 15:40:14 -0300

    add lidar pt1

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -5,20 +5,20 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.actions import TimerAction
 
 def generate_launch_description():
+    package_name = 'infinity_bot'
 
-    package_name = 'infinity_bot'  # <--- Atualize conforme o nome correto
-
-    # Lança o rsp.launch.py (robot_state_publisher)
+    # Lança o robot_state_publisher com uso de sim time
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
-        )]),
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
+        ]),
         launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Lança o Gazebo Harmonic (Ignition) com ros_gz
+    # Lança o Gazebo Harmonic (Ignition Gazebo) com mundo customizado
     gz_sim = ExecuteProcess(
         cmd=[
             'gz', 'sim',
@@ -32,21 +32,44 @@ def generate_launch_description():
         output='screen'
     )
 
+    # Spawner do modelo no Gazebo Harmonic via ros_gz_sim
+    spawn_entity = TimerAction(
+        period=5.0,
+        actions=[Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=[
+                '-name', 'my_bot',
+                '-topic', 'robot_description',
+                '-z', '0.4'
+            ],
+            output='screen'
+        )]
+    )
 
-    # Spawner via ros_gz service (para publicar o modelo no Gazebo Harmonic)
-    spawn_entity = Node(
-        package='ros_gz_sim',  # NOTA: este pacote substitui gazebo_ros para ignition
-        executable='create',
+
+    # Bridge para o tópico /cmd_vel entre ROS 2 e Gazebo
+    cmd_vel_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
         arguments=[
-            '-name', 'my_bot',
-            '-topic', 'robot_description',
-            '-z', '0.2'
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'
         ],
         output='screen'
     )
 
+    # teleop = Node(
+    #     package='teleop_twist_keyboard',
+    #     executable='teleop_twist_keyboard',
+    #     name='teleop_twist_keyboard',
+    #     output='screen',
+    #     remappings=[('/cmd_vel', '/cmd_vel')]
+    # )
+
     return LaunchDescription([
         rsp,
         gz_sim,
-        spawn_entity
+        spawn_entity,
+        cmd_vel_bridge,
+        # teleop
     ])
